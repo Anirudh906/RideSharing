a933ebe8d32c5b5b49de108fa20f7e1e12adf94f
Anirudha Shivarkar <sanirudh20019@gmail.com>
2025-07-11 10:59:05 +0530

    refactor main file

--- src/main/java/com/example/geektrust/controller/RideSharingSystem.java
+++ src/main/java/com/example/geektrust/controller/RideSharingSystem.java
@@ -11,9 +11,9 @@ import com.example.geektrust.services.impl.RideServiceImpl;
 import com.example.geektrust.services.impl.RiderServiceImpl;
 
 public class RideSharingSystem {
-  private RideService rideService;
-  private DriverService driverService;
-  private RiderService riderService;
+  private final RideService rideService;
+  private final DriverService driverService;
+  private final RiderService riderService;
 
   public RideSharingSystem() {
     this.driverService = new DriverServiceImpl();
@@ -22,31 +22,71 @@ public class RideSharingSystem {
   }
 
   public void processCommand(String command) {
-    String[] commandParts = command.split(" ");
-    switch (commandParts[0]) {
+    String[] tokens = command.split(" ");
+    String commandType = tokens[0];
+
+    switch (commandType) {
       case START_RIDE_COMMAND:
-        rideService.startRide(commandParts[1], Integer.valueOf(commandParts[2]), commandParts[3]);
+        handleStartRide(tokens);
         break;
       case STOP_RIDE_COMMAND:
-        rideService.endRide(
-            commandParts[1],
-            new Coordinates(Long.valueOf(commandParts[2]), Long.valueOf(commandParts[3])),
-            Long.valueOf(commandParts[4]));
+        handleStopRide(tokens);
         break;
       case MATCH_COMMAND:
-        rideService.printMatchedDrivers(commandParts[1]);
+        handleMatch(tokens);
         break;
       case ADD_DRIVER_COMMAND:
-        driverService.addDriver(
-            commandParts[1], Long.valueOf(commandParts[2]), Long.valueOf(commandParts[3]));
+        handleAddDriver(tokens);
         break;
       case ADD_RIDER_COMMAND:
-        riderService.addRider(
-            commandParts[1], Long.valueOf(commandParts[2]), Long.valueOf(commandParts[3]));
+        handleAddRider(tokens);
         break;
       case GET_BILL_COMMAND:
-        rideService.getRideBill(commandParts[1]);
+        handleGetBill(tokens);
+        break;
+      default:
+        System.out.println("Invalid command: " + commandType);
         break;
     }
+  }
+
+  private void handleStartRide(String[] tokens) {
+    String rideId = tokens[1];
+    int nthDriver = Integer.parseInt(tokens[2]);
+    String riderId = tokens[3];
+    rideService.startRide(rideId, nthDriver, riderId);
+  }
+
+  private void handleStopRide(String[] tokens) {
+    String rideId = tokens[1];
+    long x = Long.parseLong(tokens[2]);
+    long y = Long.parseLong(tokens[3]);
+    long time = Long.parseLong(tokens[4]);
+    Coordinates destination = new Coordinates(x, y);
+    rideService.endRide(rideId, destination, time);
+  }
+
+  private void handleMatch(String[] tokens) {
+    String riderId = tokens[1];
+    rideService.printMatchedDrivers(riderId);
+  }
+
+  private void handleAddDriver(String[] tokens) {
+    String driverId = tokens[1];
+    long x = Long.parseLong(tokens[2]);
+    long y = Long.parseLong(tokens[3]);
+    driverService.addDriver(driverId, x, y);
+  }
+
+  private void handleAddRider(String[] tokens) {
+    String riderId = tokens[1];
+    long x = Long.parseLong(tokens[2]);
+    long y = Long.parseLong(tokens[3]);
+    riderService.addRider(riderId, x, y);
+  }
+
+  private void handleGetBill(String[] tokens) {
+    String rideId = tokens[1];
+    rideService.getRideBill(rideId);
   }
 }
